d348621cdc7272535d9dc1798db5caaf488f7913
Rosebud <rkle408@gmail.com>
2022-10-24 16:48:53 -0700

    filled in missing schema and virtuals in User model

--- models/User.js
+++ models/User.js
@@ -2,20 +2,39 @@
 const mongoose = require('mongoose');
 
 // New instance of Mongoose schema:
-const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true, trim: true },
-    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
-    thoughts: [
-        //Array of _id values referencing the Thought model
-    ],
-    friends: [
-        //Array of _id values referencing the User model (self-reference)
-    ],
-});
+const userSchema = new mongoose.Schema(
+    {
+        username: { type: String, unique: true, required: true, trim: true },
+        email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+        thoughts: [
+            //Array of _id values referencing the Thought model
+            { 
+                type: Schema.Types.ObjectId,
+                ref: 'Thought', 
+            }
+        ],
+        friends: [
+            //Array of _id values referencing the User model (self-reference)
+            {
+                type: Schema.Types.ObjectId,
+            //    default: () => new Types.ObjectId(),
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
 // Compile a model based on the schema
 const User = mongoose.model('User', userSchema);
 
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
 
 module.exports = User;
