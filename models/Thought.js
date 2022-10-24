7bba978c9dbac726cc74a8a82a8c40507f582aec
Rosebud <rkle408@gmail.com>
2022-10-23 23:42:30 -0700

    added schemas and virtual to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,21 +2,32 @@
 const mongoose = require('mongoose');
 
 // Instance of schema
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
-    createdAt: { type: Date, default: Date.now },
-    // check if this is the correct time format
-    username: { type: String, required: true },
-    reactions: [
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+        createdAt: { type: Date, default: Date.now },
+        // check if this is the correct time format
+        username: { type: String, required: true },
         //Array of nested documents created with the reactionSchema
-        reactionSchema
-    ],
-    //Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-});
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
 
 //Subdocument: reaction schema
 const reactionSchema = new mongoose.Schema({
-    reactionId: , //Use Mongoose's ObjectId data type, Default value is set to a new ObjectId
+    //Use Mongoose's ObjectId data type, Default value is set to a new ObjectId
+    reactionId: { type: Schema.Types.ObjectId, ref: 'user' }, 
     reactionBody: { type: String, required: true, maxLength: 280 },
     username: { type: String, required: true },
     createdAt:  { type: Date, default: Date.now },
